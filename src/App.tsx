492318da6625ed66fd4abc18aed8f694208e13d7
cyx <735849467@qq.com>
2019-10-28 22:26:06 +0800

    fix: Nav and Drawer completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React, { useEffect, useState, ReactNode } from 'react';
+import React, { useEffect, useState, ReactNode, useCallback } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { routes } from './constant';
 import { INavItem } from './interface';
+import { Drawer } from './component/Drawer';
+import cx from 'classnames';
+
+import styles from './sass/index.module.scss';
+import './sass/reset.css';
 
 const App: React.FC = () => {
+  const [navItemActive, setActive] = useState<number>(0);
+  const handleNavChange = useCallback(
+    (index: number) => () => setActive(index),
+    [setActive]
+  );
+
   const renderNav = (elem: INavItem, index: number): ReactNode => {
     const { urlMatch, name } = elem;
 
     return (
       <>
-        <Link to={urlMatch} key={index}>
+        <Link
+          to={urlMatch}
+          key={index}
+          className={cx({
+            [styles.navLink]: true,
+            [styles.active]: index === navItemActive
+          })}
+          onClick={handleNavChange(index)}
+        >
           {name}
         </Link>
       </>
@@ -40,7 +59,9 @@ const App: React.FC = () => {
     <div className="App">
       <Router>
         <nav>
-          <div>{routes.map((elem, index) => renderNav(elem, index))}</div>
+          <Drawer width={'200px'} padding={'30px 0'}>
+            <div>{routes.map((elem, index) => renderNav(elem, index))}</div>
+          </Drawer>
         </nav>
         <Switch>
           <div>{routes.map((elem, index) => renderNavItem(elem, index))}</div>
